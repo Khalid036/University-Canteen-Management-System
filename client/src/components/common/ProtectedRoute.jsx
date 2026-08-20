1b43112db7ee543c03a20828547b25411a1f692d
Khalid036 <khalid036saifullah@gmail.com>
2026-08-20 22:17:54 +0600

    Created ProtectedRoute

--- client/src/components/common/ProtectedRoute.jsx
+++ client/src/components/common/ProtectedRoute.jsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuthStore } from '../../store/authStore';
 
-export const ProtectedRoute = () => {
-  const { isAuthenticated, user } = useAuthStore();
-
-  if (!isAuthenticated || !user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return <Outlet />;
-};
